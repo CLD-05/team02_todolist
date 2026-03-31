dabcd5ff03351c14a54e58077d7bafd33ad98241
jangmh1017 <jangmh697@gmail.com>
2026-03-31 11:08:55 +0900

    feat: add security config

--- todolist/src/main/java/com/team/todolist/config/SecurityConfig.java
+++ todolist/src/main/java/com/team/todolist/config/SecurityConfig.java
@@ -1,5 +1,43 @@
 package com.team.todolist.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+	@Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                // URL 접근 권한 설정
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/users/join", "/login", "/css/**", "/js/**", "/images/**").permitAll() // 로그인 없이 접속 가능
+                        .requestMatchers("/todos/**").authenticated() // 로그인 후 접속 가능
+                        .anyRequest().authenticated()
+                )
+
+                // 로그인 설정
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/todos", true)
+                        .permitAll()
+                )
+
+                // 로그아웃 설정
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                );
+
+        return http.build();
+    }
 
 }
