f18d45435a1bcb986917e823bb847fdf4e983e76
WONJUN6 <cnfdornqrl20@gamil.com>
2026-03-31 11:28:35 +0900

    feat : security-auth 추가

--- todolist/src/main/java/com/team/todolist/user/entity/User.java
+++ todolist/src/main/java/com/team/todolist/user/entity/User.java
@@ -1,5 +1,35 @@
 package com.team.todolist.user.entity;
 
-public class User {
+import com.team.todolist.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Builder
+    public User(String email, String password, String role) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
