77f21259abcf45a3d3e82d1b471ec0d30ecf66c1
hochang1 <lhc4254@gmail.com>
2026-03-31 16:33:11 +0900

    feat: merge_first

--- todolist/src/main/java/com/team/todolist/config/SecurityConfig.java
+++ todolist/src/main/java/com/team/todolist/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.team.todolist.config;
 
+import lombok.RequiredArgsConstructor;
+import com.team.todolist.security.service.CustomUserDetailsService;
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+	private final CustomUserDetailsService customUserDetailsService;
+	
+	http.userDetailsService(customUserDetailsService);
 }
--- todolist/src/main/java/com/team/todolist/todo/controller/TodoController.java
+++ todolist/src/main/java/com/team/todolist/todo/controller/TodoController.java
@@ -1,5 +1,17 @@
 package com.team.todolist.todo.controller;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import com.team.todolist.security.auth.CustomUserDetails;
 
+@AuthenticationPrincipal CustomUserDetails userDetails
 public class TodoController {
 
+	User user = null;
+	if (userDetails != null) {
+	    user = userDetails.getUser();
+	}
+	
+	User user = null;
+	if (userDetails != null) {
+	    user = userDetails.getUser();
+	}
 }
